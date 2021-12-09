c7c5488eaee2af0d4f30796e3a7c8da7e8319525
yomogi <tsuyomogi@gmail.com>
2021-12-09 14:30:43 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+[![gh-pages](https://img.shields.io/static/v1?label=GitHub+Pages&message=+&color=brightgreen&logo=github)](https://moyomogi.github.io/web/)
+[![actions-status](https://github.com/moyomogi/web/actions/workflows/cd.yml/badge.svg)](https://github.com/moyomogi/web/actions/workflows/cd.yml)
+[![last-commit](https://img.shields.io/github/last-commit/moyomogi/web)](https://github.com/moyomogi/web/commits/master)
+[![license](https://img.shields.io/badge/license-CC0-blue)](https://creativecommons.org/publicdomain/zero/1.0/deed.ja)
+# Vue Testing
+> Nuxt.js を用いた Vue.js のテストです。  
+
+![arona](https://i.imgur.com/ta8QePb.png)  
+
+
+## How to Set Up
+(Windows 向けに書かれています。Windows 以外の OS については知りません(無責任)。)  
+
+* WSL (Windows Subsystem for Linux) を入れる。
+  [Windows 10でLinuxを使う(WSL2)](https://qiita.com/whim0321/items/ed76b490daaec152dc69) を参考に。  
+
+* Nuxt.js のインストール。
+  (Nuxt.js とは、[Vue.js](https://v3.ja.vuejs.org/) を用いて作られた、ウェブサイトのテンプレートです。)  
+  [Nuxt.js の公式インストールガイド](https://nuxtjs.org/ja/docs/get-started/installation/) に従い、 WSL 上で `npm init nuxt-app@latest <my-project>` をします。  
+
+* 最後に `npm run dev` とすると、 [http://localhost:3000](http://localhost:3000) で見えるようになります。
+
+
+## Others
+UI は [Oruga](https://oruga.io) という UI テンプレートを使っています。
---
+++ pages/index.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="container">
+    <head>
+      <meta property="og:title" content="Vue Testing"/>
+      <meta property="og:description" content="Vue.js のテスト。"/>
+      <meta property="og:image" content="https://i.imgur.com/yHdEbDN.png"/>
+    </head>
+    <!-- <img class="logo" src="~/static/arona.png"> -->
+    <img class="logo" src="https://i.imgur.com/yHdEbDN.png">
+    <router-link class="routerLink" to="/">
+      <h1 class="title">Vue Testing</h1>
+    </router-link>
+
+    <p class="subtitle">テスト。</p>
+    <div class="buttons">
+      <o-button root-class="button" variant-class="button-" variant="light" @click="goToGithub()">
+        GitHub
+      </o-button>
+      <o-button root-class="button" variant-class="button-" variant="dark" @click="goToCommits()">
+        Commits
+      </o-button>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'HomePage',
+  methods: {
+    goToGithub () {
+      // @ts-ignore
+      window.location = 'https://github.com/moyomogi/web';
+    },
+    goToCommits () {
+      // @ts-ignore
+      window.location = 'https://github.com/moyomogi/web/commits/master'
+    }
+  }
+}
+
+</script>
+
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  flex-direction: column;
+}
+
+.logo {
+  max-width: 240px;
+}
+
+.routerLink {
+  text-decoration: none;
+}
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 200;
+  font-size: 60px;
+  color: #5facff;
+  letter-spacing: 1px;
+  text-decoration: none;
+}
+.subtitle {
+  font-weight: 200;
+  font-size: 20px;
+  color: #526488;
+  word-spacing: 2px;
+  padding-bottom: 24px;
+}
+
+.o-button {
+  padding: 10px 20px;
+}
+.buttons {
+  padding-top: 9px;
+}
+.button {
+  padding: 9px;
+  min-width: 130px;
+}
+.button-light {
+  background-color: #3cb1c5;
+}
+.button-dark {
+  background-color: #293923;
+}
+
+</style>
